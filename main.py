bb5a12d25ebee7924fc0b8370df84a1589b435d3
Bloodouts <88408451+Bloodouts@users.noreply.github.com>
2022-04-07 16:56:12 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-#
-# Added coded in main(),and create a function to ask for your name and return it.
-#
-
 from start_adventure import *
-from player import *
 from combat import *
 
 
@@ -13,40 +8,10 @@ def main():
     # '''
     game = Game()
 
-    #game.populate()
     print("Welcome, adventurer.")
     game.list_commands()
     game.hero.name_self()
     game.hero.look_self()
-    #game.look()
-
-    # while True:
-    #     game.handle_input()
-    #     game.update()
-    #     game.output()
-
-    ####################################################################
-    # ACTIVITIES
-    #
-    # Read some of the best practices when writing Python code
-    #   http://legacy.python.org/dev/peps/pep-0008/
-    # Main thing is if you are using tabs, make sure it's 4-spaces,
-    # most editors will convert it (check preferences/settings).
-    #
-    # Modify the code
-    # - add taunting the guard or talking
-    # - sword fight with the guard, and keep track of health points (HP)
-    # - puzzles like 1+2 during an encounter
-    # - modify blue_door_room()'s if statement
-    #   so it takes into account player typing "right" or "guard"
-    #   Hint: Add another elif before the else statement
-    #
-    # So many if statements, this can be made simpler and easier to
-    # maintain by using Finite State Machine (FSM)
-    # You can find info about it, but it will mainly be touching
-    # object-orient programming, which is another lesson for another day.
-    #
-    #####################################################################
 
     start_adventure()
 
